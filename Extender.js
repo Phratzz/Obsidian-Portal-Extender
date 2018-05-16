af17263e98cda29322ed697d6460585c8cecd13a
Anders Larsen <anders.larsen5777@gmail.com>
2018-05-16 18:21:03 +0200

    Update Extender.js

--- Extender.js
+++ Extender.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function() {
-	$(document).ready(function() {
+	jQuery(document).ready(function() {
 		/* Extra Functions */
 		Array.prototype.clean = function(deleteValue) {
 			for (var i = 0; i < this.length; i++) {
@@ -15,20 +15,20 @@ window.addEventListener("load", function() {
 		var $groups = new Array('Class', 'Creature', 'Race', 'God', 'Job', 'Location', 'Sex');
 		var $tags = new Array();
 	
-		$('.filter-options .tag-check-list .row div').each(function() {
-			$(this).children("label").each(function() {
-				$tags.push($(this));
+		jQuery('.filter-options .tag-check-list .row div').each(function() {
+			jQuery(this).children("label").each(function() {
+				$tags.push(jQuery(this));
 			});
 		});
 		
 		/* Insert category for each group */
-		var $filters = $('.filter-options form.custom');
-		$.each($groups, function($groupKey, $groupValue) {
+		var $filters = jQuery('.filter-options form.custom');
+		jQuery.each($groups, function($groupKey, $groupValue) {
 			$filters.append('<div class="row underlined custom-underlined"><div class="large-12 columns">' + $groupValue + '</div></div>');
 			$filters.append('<div class="tag-custom-check-list" filter="' + $groupValue + '"><div class="row"><div class="large-3 columns"></div><div class="large-3 columns"></div><div class="large-3 columns"></div><div class="large-3 columns"></div></div></div>');
 			
 			var $myTags = new Array();
-			$.each($tags, function($tagKey, $tagValue) {
+			jQuery.each($tags, function($tagKey, $tagValue) {
 				if($tagValue.children('input').data('tag').split(':')[0] == $groupValue) {
 					$myTags.push($tagValue);
 					delete $tags[$tagKey];
@@ -37,58 +37,58 @@ window.addEventListener("load", function() {
 			$tags.clean(undefined);
 			
 			var $myFilter = $filters.children('.tag-custom-check-list[filter="' + $groupValue + '"]');
-			$.each($myTags, function($tagKey, $tagValue) {
+			jQuery.each($myTags, function($tagKey, $tagValue) {
 				$myFilter.children('.row').children('.columns:nth-child(' + (($tagKey%4)+1) + ')').append($tagValue);
 			});
 		});
 		
 		/* Insert category for each group */
-		$('.tag-custom-check-list label').each(function() {
-			var $delimiter = $(this).parents('.tag-custom-check-list').attr('filter') + ": ";
-			var $html = $(this).html();
+		jQuery('.tag-custom-check-list label').each(function() {
+			var $delimiter = jQuery(this).parents('.tag-custom-check-list').attr('filter') + ": ";
+			var $html = jQuery(this).html();
 			var $htmlArray = $html.split($delimiter);
 			var $lastVar = $htmlArray.pop();
 			var $restVar = $htmlArray.join($delimiter);
-			$(this).html($restVar + $lastVar);
+			jQuery(this).html($restVar + $lastVar);
 		});
 		
 		/* Insert category fof remaining tags */
 		$filters.append('<div class="row underlined custom-underlined"><div class="large-12 columns">Other</div></div>');
 		$filters.append('<div class="tag-custom-check-list" filter="Other"><div class="row"><div class="large-3 columns"></div><div class="large-3 columns"></div><div class="large-3 columns"></div><div class="large-3 columns"></div></div></div>');
 		var $myFilter = $filters.children('.tag-custom-check-list[filter="Other"]');
-		$.each($tags, function($tagKey, $tagValue) {
+		jQuery.each($tags, function($tagKey, $tagValue) {
 			$myFilter.children('.row').children('.columns:nth-child(' + (($tagKey%4)+1) + ')').append($tagValue);
 		});
 		
 		/* Remove and unbind old stuff not needed anymore */
-		$('form.custom>.underlined:not(".custom-underlined")').remove();
-		$('form.custom .tag-check-list').remove();
-		$('form.custom *').unbind();
+		jQuery('form.custom>.underlined:not(".custom-underlined")').remove();
+		jQuery('form.custom .tag-check-list').remove();
+		jQuery('form.custom *').unbind();
 		
 		/* Add tags toggle events */
-		$('.tag-custom-check-list label').click(function() {
-			if($(this).attr("data-method") == "Add") {
-				$(this).attr("data-method", "Sub");
-			} else if($(this).attr("data-method") == "Sub") {
-				$(this).removeAttr("data-method");
+		jQuery('.tag-custom-check-list label').click(function() {
+			if(jQuery(this).attr("data-method") == "Add") {
+				jQuery(this).attr("data-method", "Sub");
+			} else if(jQuery(this).attr("data-method") == "Sub") {
+				jQuery(this).removeAttr("data-method");
 			} else {
-				$(this).attr("data-method", "Add");
+				jQuery(this).attr("data-method", "Add");
 			}
 			return false;
 		});
 		
 		/* Isotope event */
-		var $grid = $('.character-list').find('.character-list-item-container');
+		var $grid = jQuery('.character-list').find('.character-list-item-container');
 		var $filters = {};
-		$.each($groups, function($groupKey, $groupValue) {
+		jQuery.each($groups, function($groupKey, $groupValue) {
 			$filters[$groupValue] = {};
 		});
 		$filters['Other'] = {};
 		
-		$('.tag-custom-check-list label').click(function() {
-			var $filterGroup = $(this).parents('.tag-custom-check-list').attr('filter');
-			var $filterElement = $(this).children('input').attr('data-tag');
-			var $filterOption = $(this).attr('data-method');
+		jQuery('.tag-custom-check-list label').click(function() {
+			var $filterGroup = jQuery(this).parents('.tag-custom-check-list').attr('filter');
+			var $filterElement = jQuery(this).children('input').attr('data-tag');
+			var $filterOption = jQuery(this).attr('data-method');
 			$filters[$filterGroup][$filterElement] = $filterOption;
 			if($filterOption == undefined) {
 				delete $filters[$filterGroup][$filterElement];
@@ -99,39 +99,39 @@ window.addEventListener("load", function() {
 			delete $filterElement;
 			delete $filterOption;
 		});
-		$('.custom-filter-options label input').on('change', function() {
+		jQuery('.custom-filter-options label input').on('change', function() {
 			customFilter($filters);
 		});
-		$('#quick_search').on('keyup paste', function() {
+		jQuery('#quick_search').on('keyup paste', function() {
 			customFilter($filters);
 		});
 		
 		//$grid.isotope({ filter: '.show-in-filter:not(.hide-in-filter)' });
 		/* Custom isotope event */
 		function customFilter($filters) {
-			$('.character-list .character-list-item-container').each(function() {
-				var $character = $(this);
+			jQuery('.character-list .character-list-item-container').each(function() {
+				var $character = jQuery(this);
 				var $isSet = false;
 				$character.removeClass('show-in-filter');
 				$character.removeClass('hide-in-filter');
 				
-				if($('#quick_search').val() != "") {
+				if(jQuery('#quick_search').val() != "") {
 					$isSet = true;
-					if($character.find('.character-name a').text().toLowerCase().indexOf($('#quick_search').val().toLowerCase()) != -1) {
+					if($character.find('.character-name a').text().toLowerCase().indexOf(jQuery('#quick_search').val().toLowerCase()) != -1) {
 						$character.addClass('show-in-filter');
 					}
 				}
-				var $value = $('#quick_search').val();
+				var $value = jQuery('#quick_search').val();
 				
 				var $hasFilter = false;
 				var $filterExists = false;
-				if($('.pc-npc-filter-options label[for="pc"] input').is(':checked')) {
+				if(jQuery('.pc-npc-filter-options label[for="pc"] input').is(':checked')) {
 					$filterExists = true;
 					if($character.hasClass('pc')) {
 						$hasFilter = true;
 					}
 				}
-				if($('.pc-npc-filter-options label[for="npc"] input').is(':checked')) {
+				if(jQuery('.pc-npc-filter-options label[for="npc"] input').is(':checked')) {
 					$filterExists = true;
 					if($character.hasClass('npc')) {
 						$hasFilter = true;
@@ -147,13 +147,13 @@ window.addEventListener("load", function() {
 				
 				var $hasFilter = false;
 				var $filterExists = false;
-				if($('.public-gm-only-filter-options label[for="public"] input').is(':checked')) {
+				if(jQuery('.public-gm-only-filter-options label[for="public"] input').is(':checked')) {
 					$filterExists = true;
 					if(!$character.children('div').hasClass('gm-only')) {
 						$hasFilter = true;
 					}
 				}
-				if($('.public-gm-only-filter-options label[for="gm_only"] input').is(':checked')) {
+				if(jQuery('.public-gm-only-filter-options label[for="gm_only"] input').is(':checked')) {
 					$filterExists = true;
 					if($character.children('div').hasClass('gm-only')) {
 						$hasFilter = true;
@@ -167,10 +167,10 @@ window.addEventListener("load", function() {
 					$isSet = true;
 				}
 				
-				$.each($filters, function($filterKey, $filerValue) {
+				jQuery.each($filters, function($filterKey, $filerValue) {
 					var $hasFilter = false;
 					var $filterExists = false;
-					$.each($filerValue, function($elementKey, $elementValue) {
+					jQuery.each($filerValue, function($elementKey, $elementValue) {
 						if($elementValue == "Add") { $filterExists = true; }
 						if(($elementValue == "Add") && (!$isSet || $character.hasClass('show-in-filter'))) {
 							if($character.find('.tags-list a[data-tag="' + $elementKey + '"]').attr('data-tag') == $elementKey) {
@@ -199,10 +199,10 @@ window.addEventListener("load", function() {
 			delete $isSet;
 			
 			$grid.each(function() {
-				$(this).hide();
-				if($(this).hasClass("show-in-filter")) {
-					if(!$(this).hasClass("hide-in-filter")) {
-						$(this).show();
+				jQuery(this).hide();
+				if(jQuery(this).hasClass("show-in-filter")) {
+					if(!jQuery(this).hasClass("hide-in-filter")) {
+						jQuery(this).show();
 					}
 				}
 			});
